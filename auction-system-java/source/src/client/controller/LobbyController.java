762329a82da6385a820c5c8a76fa93abc758d030
Pham Phan Nhat Lam <25021841@vnu.edu.vn>
2026-04-24 13:56:40 +0700

    update khung lobbyview controller

--- auction-system-java/source/src/client/controller/LobbyController.java
+++ auction-system-java/source/src/client/controller/LobbyController.java
@@ -1,4 +1,19 @@
 package client.controller;
 
+import javafx.fxml.FXML;
+import javafx.event.ActionEvent;
+
 public class LobbyController {
+
+    @FXML private void handleLogout(ActionEvent e) {}
+    @FXML private void handleSearch() {}
+    @FXML private void handleSelectAuction() {}
+    @FXML private void handleCreateAuction(ActionEvent e) {}
+    @FXML private void handleRefresh(ActionEvent e) {}
+    @FXML private void handleQuickBid50(ActionEvent e) {}
+    @FXML private void handleQuickBid100(ActionEvent e) {}
+    @FXML private void handleQuickBid500(ActionEvent e) {}
+    @FXML private void handlePlaceBid(ActionEvent e) {}
+    @FXML private void handleCancelAuction(ActionEvent e) {}
+    @FXML private void handleClearLog(ActionEvent e) {}
 }
